190f4b9e44aa6f3491e4a60da7fcab3e2a9a5712
Wasim Sandhu <sandhuwasim@gmail.com>
2023-09-11 18:46:16 -0700

    new: Create Toolbox class

--- rosalind/wasims_toolbox.py
+++ rosalind/wasims_toolbox.py
@@ -3,6 +3,33 @@
 # Description: Collection of functions authored for
 # various Rosalind problems and commonly reused
 
+class Toolbox:
+    
+    def __init__(self) -> None:
+        self.rna_codon_table = {
+            'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',
+            'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',
+            'UAU': 'Y', 'UAC': 'Y', 'UAA': 'Stop', 'UAG': 'Stop',
+            'UGU': 'C', 'UGC': 'C', 'UGA': 'Stop', 'UGG': 'W',
+            'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+            'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+            'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+            'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+            'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',
+            'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',
+            'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',
+            'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',
+            'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+            'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+            'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+            'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G'
+        }
+    
+    def translate_codon(self, codon: str):
+        """Returns the amino acid that corresponds to the given RNA codon."""
+        return self.rna_codon_table[codon]
+
+
 def read_fasta(sequences: str=None, file_path: str=None) -> dict:
     
     """
